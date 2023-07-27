19976360ea844cb5fa5da27562c7dbe404db9fda
Vladimir <svami.vd@gmail.com>
2023-07-27 14:46:17 +0400

    add code in second view

--- DZ Switch/DZ Switch/View/ViewController2.swift
+++ DZ Switch/DZ Switch/View/ViewController2.swift
@@ -9,37 +9,109 @@ import UIKit
 
 class ViewController2: UIViewController {
 
-    let lab = UILabel()
+    let fioLabel = StyleLabel()
+    let fioTextField = StyleTextField()
     
+    let guestsLabel = StyleLabel()
+    let guestsTextField = StyleTextField()
+    
+    let tableLabel = StyleLabel()
+    let tableTextField = StyleTextField()
+    
+    let reservLabel = StyleLabel()
+    let reservSwitch = UISwitch()
+    
+    let prepaidLabel = StyleLabel()
+    let prepaidSwitch = UISwitch()
+    
+    let vipLabel = StyleLabel()
+    let vipSwitch = UISwitch()
+    
+    let billButton = StyleButtom()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        MARK: - title
         
-        self.view.backgroundColor = .systemPink
+        self.title = "Cafe Mario"
         
-        self.title = "mVC"
+//        MARK: - fio
+        
+        fioLabel.styleAndPosition(textLabel: "Фио", y: 130)
+        self.view.addSubview(fioLabel)
+        
+        fioTextField.styleAndPosition(textPlaceholder: "Введите ФИО", y: 170, secureText: false)
+        self.view.addSubview(fioTextField)
+        
+//        MARK: - guests
+        
+        guestsLabel.styleAndPosition(textLabel: "Количество гостей", y: 230)
+        self.view.addSubview(guestsLabel)
+        
+        guestsTextField.styleAndPosition(textPlaceholder: "Введите количество", y: 270, secureText: false)
+        self.view.addSubview(guestsTextField)
+        
+//        MARK: - guests
 
-        lab.backgroundColor = .yellow
-        lab.text = "Shalom, epta :)"
+        tableLabel.styleAndPosition(textLabel: "Номер стола", y: 330)
+        self.view.addSubview(tableLabel)
         
-        lab.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
-        lab.textColor = .blue
-        lab.textAlignment = .center
-        lab.font = lab.font.withSize(18)
+        tableTextField.styleAndPosition(textPlaceholder: "Стол номер", y: 370, secureText: false)
+        self.view.addSubview(tableTextField)
         
-        self.view.addSubview(lab)
+//        MARK: - reservation
+        
+        reservLabel.styleAndPosition(textLabel: "Бронировали стол?", y: 460)
+        self.view.addSubview(reservLabel)
+
+        reservSwitch.frame = CGRect(x: 300, y: 470, width: 100, height: 70)
+        self.view.addSubview(reservSwitch)
+        
+//        MARK: - prepaid
+        
+        prepaidLabel.styleAndPosition(textLabel: "Предоплата?", y: 530)
+        self.view.addSubview(prepaidLabel)
+        
+        prepaidSwitch.frame = CGRect(x: 300, y: 540, width: 100, height: 70)
+        self.view.addSubview(prepaidSwitch)
+        
+//        MARK: - vip room
+        
+        vipLabel.styleAndPosition(textLabel: "VIP комната?", y: 610)
+        self.view.addSubview(vipLabel)
+        
+        vipSwitch.frame = CGRect(x: 300, y: 620, width: 100, height: 70)
+        self.view.addSubview(vipSwitch)
+        
+//        MARK: - bill button
+        
+        billButton.styleAndPosition(text: "Выставить счет?", x: 40, y: 700, width: 310, color: .systemPink)
+        self.view.addSubview(billButton)
+        billButton.addTarget(self, action: #selector(bill(sender:)), for: .touchUpInside)
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func bill(sender: Any) {
+        
+        let billAlert = UIAlertController(title: "Выставить счет?", message: nil, preferredStyle: .alert)
+        
+        if fioTextField.text!.isEmpty || guestsTextField.text!.isEmpty || tableTextField.text!.isEmpty {
+            billAlert.message = "Заполнены не все поля. Отменить?"
+        }
+        
+        let okActionAlert = UIAlertAction(title: "Чек", style: .default) { (action) in
+            let thirdController = ViewController3()
+            self.navigationController?.pushViewController(thirdController, animated: true)
+        }
+        
+        let cancelActionAlert = UIAlertAction(title: "Отмена", style: .cancel)
+        
+        billAlert.addAction(okActionAlert)
+        billAlert.addAction(cancelActionAlert)
+        
+        self.present(billAlert, animated: true)
+        
     }
-    */
-
+    
 }
