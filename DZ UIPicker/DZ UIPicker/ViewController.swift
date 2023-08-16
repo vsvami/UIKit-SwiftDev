8a41282fe66c40bc88cb1f233229f234cc18fa91
Vladimir <svami.vd@gmail.com>
2023-08-16 21:09:57 +0400

    add DZ UIPicker folder and first view

---
+++ DZ UIPicker/DZ UIPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ UIPicker
+//
+//  Created by Владимир Дмитриев on 13.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
